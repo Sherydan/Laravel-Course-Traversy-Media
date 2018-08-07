c2e65eacf710e2c5056f9b3fc75cf23aa2daa8ed
Luis Tobar <tobar.luis.c@gmail.com>
2018-08-06 21:35:41 -0400

    Routes and Controllers

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+# pasar parametros a la vista
+Route::get('users/{id}/{name}', function($id, $name){
+    return 'This is user id: '. $id.' And his name is: '. $name;
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+*/
+
+# PAGES ROUTES
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+
+
